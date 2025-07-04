28f12f46d67b4b9e832f14434e8c072b5351f45a
Ramona <ramonath@icloud.com>
2025-07-04 16:21:25 +0200

    Stand 04.07.25 Mockoon Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProductModule } from './product/product.module'; // << hinzufügen
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+;
+import { CartModule } from './cart/cart.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // MatCardModule,
+    ProductModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    CartModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
