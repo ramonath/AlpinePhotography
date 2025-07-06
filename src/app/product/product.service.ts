25596c82fb4a15999b8ee0acf6da38bb9d03e414
Ramona <ramonath@icloud.com>
2025-07-06 18:13:16 +0200

    Stand 06.07.25

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,24 +1,16 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
 import { Product } from '../models/product';
-import { SupabaseService } from '../supabase.service'; // Pfad ggf. anpassen
+import { SupabaseService } from '../supabase.service'; 
 
 import { map } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  
-
-  constructor(
-    private http: HttpClient,
-    private supabaseService: SupabaseService
-  ) { }
+  constructor(private supabaseService: SupabaseService) {}
 
   getProducts(): Observable<Product[]> {
     const supabase = this.supabaseService.getSupabaseClient();
